fb2d32ab3677d505cc80b246c868d89e301a1608
Guillaume MODARD <guillaumemodard@gmail.com>
2023-12-12 10:28:42 +0100

    Update constant documentation

--- pebbleauthclient/constants.py
+++ pebbleauthclient/constants.py
@@ -22,5 +22,11 @@ env_exp = os.getenv('PBL_JWKS_EXP_TIME')
 JWKS_EXP_TIME: int = int(env_exp) if env_exp else 86400
 """
 Duration in seconds after which Keys Set (JWKS) is considered as expired. All local copy of the keys must be destroyed 
-and the remote server will be requested to create the new copy.
+and the remote server will be requested to create the new copy. Default value : 86400 sec (one day). This can be changed
+by exporting `PBL_JWKS_EXP_TIME` environment variable.
+
+::
+
+    export PBL_JWKS_EXP_TIME=3600
+
 """
